faf9573532cb0314eb35082937ee0d5ebe17ba84
Pavel Semchukov <7pd@mail.ru>
2025-01-27 16:40:59 +0300

    feat:тестовый проект

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public float speed = 5.0f; // Скорость перемещения объекта
+
+    void Update()
+    {
+        // Получаем ввод с клавиатуры
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Создаем вектор перемещения
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Перемещаем объект
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
+
